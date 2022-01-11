56a1e5a42bfec312438fbcf13d188862589db1a4
VaniushaR <vaniusha.ra@gmail.com>
2022-01-11 11:13:58 -0600

    testing with jest

---
+++ MyFirstTest/index.js
@@ -0,0 +1,9 @@
+//func que sume 1 numero a otro y retorne el resultado
+
+const sum = (a, b) => {
+      return a + b
+     }
+
+module.exports = sum
+
+//console.log(module)
---
+++ MyFirstTest/index.spec.js
@@ -0,0 +1,7 @@
+const sum = require('./index.js')
+
+test('should show the result of the sum', () => {
+      expect(sum(1,2)).toBe(3)
+})
+
+
